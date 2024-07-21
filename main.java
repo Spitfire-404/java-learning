f045a780e616e7b659c61dd573638fc31dfc68a7
Spitfire-404 <saltenisu@gmail.com>
2024-07-21 04:57:30 -0400

    starting a new branch
    probably gonna experiment with the game idea further solo

--- main.java
+++ main.java
@@ -1,13 +1,14 @@
-// main java file
 public class main { 
+    
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         init();
     }
     private static void init() {
         renderer window = new renderer(800, 600); // make a new window
-        window.draw(new int[]{10, 10, 300, 300,0,100});
-        window.draw(new int[]{300, 300,0,100});
+        window.drawPoly(new int[]{10, 10, 300, 300,0,100});
         
     }
+
+    
 }
--- renderer.java
+++ renderer.java
@@ -1,17 +1,25 @@
 // stuff for jframes?
 import java.awt.*;
+import java.io.File;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class renderer extends JFrame{
-
+    private int width;
+    private int height;
     renderer(int width, int height) {
         super("window");
-        setSize(480, 200);
+        this.width = width;
+        this.height = height;
+        setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        loadImg();
     }
     private int[] points;
+    private ImageIcon[] images;
     public Graphics2D g2d;
     public void paint(Graphics g) {
         super.paint(g);
@@ -20,10 +28,26 @@ public class renderer extends JFrame{
         for(int i = 0; i < points.length; i += 2) {
             g2d.drawLine(points[i], points[i+1], points[i+2], points[i+3]);
         }
+            g.drawImage(img, 0,0, this);
 }
-    void draw(int[] pointsToDraw) {
+    void drawPoly(int[] pointsToDraw) {
         points = pointsToDraw;
         repaint();
+    }
+    void drawImg(ImageIcon[] imagesToDraw) {
+        images = imagesToDraw;
+        repaint();
+    }
+
+    Image img;
+    private void loadImg() {
+        try{
+            img = ImageIO.read(new File("Chad.jpg"));// new ImageIcon("Chad.jpg").getImage();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        //return img;
     }
 
 }
