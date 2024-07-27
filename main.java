d476add87ca1061a1870e1341a0a6d57a3c5248b
Spitfire-404 <saltenisu@gmail.com>
2024-07-27 02:32:56 -0400

    commenting and cleaning up

--- main.java
+++ main.java
@@ -1,26 +1,21 @@
 // main java file
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import javax.imageio.*;
 
 public class main { 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello, World!");
-        init();
-
-
-        
+        init(); 
     }
     private static void init() throws IOException {
         BufferedImage img = ImageIO.read(new File("Chad.jpg"));// choose an image to draw
         renderer renderer = new renderer(800, 600); // make a new window
-        //                                                                             |                                                                     |
-        //                                                                             v NOTE: think of input as a buffer, any previous input is overwritten v
-        renderer.drawImg(new BufferedImage[]{img, img}, new int[][]{{0,0},{300,300}}); // draw the image
+//                                                                                    |                                                                     |
+//                                                                                    v NOTE: think of input as a buffer, any previous input is overwritten v
+        renderer.drawImg(new BufferedImage[]{img, img}, new int[][]{{0,0},{300,300}}); // draw the image (first an image array, second an array of positions)
         renderer.drawPoly(new int[]{100, 100, 200, 100, 200, 200, 100, 200, 100, 100}); // draw lines
-        renderer.setVisible(true);
-        renderer.refresh();
+        renderer.refresh();// manually call the repaint method
     }
 }
--- renderer.java
+++ renderer.java
@@ -4,48 +4,61 @@ import java.awt.image.BufferedImage;
 import javax.swing.*;
 
 public class renderer extends JFrame{
-
-
+    // width and height of the window for reference
     private int width;
     private int height;
+
+    // constructor
     renderer(int width, int height) {
-        super("window");
-        this.width = width;
-        this.height = height;
-        setSize(width, height);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        super("window");//use the jframe constructor to create and name the window
+        this.width = width; // set the width
+        this.height = height; // set the height
+        setSize(width, height); // set the size of the window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // ???
+        setLocationRelativeTo(null); // set the window in the middle of the screen
+        setVisible(true); // make the window visible
     }
-    private int[] points;
-    public Graphics2D g2d;
+
+    private int[] points; // points to draw
+    public Graphics2D g2d; // graphics2d object (the thing that draws the lines and images)
+
+    // override the paint method
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
-        g2d = (Graphics2D) g;
+        super.paint(g); // call the paint method of the super class
+        g2d = (Graphics2D) g; // cast the graphics object to a graphics 2d object
+        
+        // draw the lines
         for(int i = 0; i < points.length-2; i += 2) {
             g2d.drawLine(points[i], points[i+1], points[i+2], points[i+3]);
         }
+
+        // draw the images
         for(int i = 0; i < images.length; i++) {
-            g.drawImage(images[i], imagesPos[i][0], imagesPos[i][1], null);
+            g2d.drawImage(images[i], imagesPos[i][0], imagesPos[i][1], null);
         }
     }
     
+
+    // methods to draw lines
     void drawPoly(int[] pointsToDraw) {
-        points = pointsToDraw;
-        repaint();
+        points = pointsToDraw; // overwrite the points array
+        repaint(); //call the repaint method
     }
 
-    BufferedImage[] images;
-    int[][] imagesPos;
+    BufferedImage[] images; // images to draw
+    int[][] imagesPos; // positions of the images to draw
+
+    // methods to draw images
     void drawImg(BufferedImage[] imagesToDraw, int[][] pos) {
-        this.imagesPos = pos;
-        this.images = imagesToDraw;
-        repaint();
+        this.imagesPos = pos; // overwrite the imagesPos array
+        this.images = imagesToDraw; // overwrite the images array
+        repaint(); // call the repaint method
     }
 
+    // a method to manually call the repaint method
     void refresh() {
-        repaint();
+        repaint(); // call the repaint method
     }
 
 }
