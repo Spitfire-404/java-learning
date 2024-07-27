99c72ef0134ed494e32e23eb28c892a77b6f2928
Spitfire-404 <saltenisu@gmail.com>
2024-07-27 00:06:49 -0400

    Images are now working

--- main.java
+++ main.java
@@ -1,11 +1,9 @@
 // main java file
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.*;
-import javax.swing.JPanel;
 
 public class main { 
     public static void main(String[] args) throws IOException {
@@ -16,16 +14,13 @@ public class main {
         
     }
     private static void init() throws IOException {
-        final BufferedImage img = ImageIO.read(new File("Chad.jpg"));// new ImageIcon("Chad.jpg").getImage();
+        BufferedImage img = ImageIO.read(new File("Chad.jpg"));// choose an image to draw
         renderer renderer = new renderer(800, 600); // make a new window
-        JPanel pane = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(img, 0, 0, null);
-            }
-        };
-        renderer.add(pane);
+        //                                                                             |                                                                     |
+        //                                                                             v NOTE: think of input as a buffer, any previous input is overwritten v
+        renderer.drawImg(new BufferedImage[]{img, img}, new int[][]{{0,0},{300,300}}); // draw the image
+        renderer.drawPoly(new int[]{100, 100, 200, 100, 200, 200, 100, 200, 100, 100}); // draw lines
         renderer.setVisible(true);
+        renderer.refresh();
     }
 }
--- renderer.java
+++ renderer.java
@@ -4,6 +4,8 @@ import java.awt.image.BufferedImage;
 import javax.swing.*;
 
 public class renderer extends JFrame{
+
+
     private int width;
     private int height;
     renderer(int width, int height) {
@@ -14,29 +16,36 @@ public class renderer extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
-
-        loadImg();
     }
     private int[] points;
     public Graphics2D g2d;
+    @Override
     public void paint(Graphics g) {
         super.paint(g);
         g2d = (Graphics2D) g;
-        
-
-}
+        for(int i = 0; i < points.length-2; i += 2) {
+            g2d.drawLine(points[i], points[i+1], points[i+2], points[i+3]);
+        }
+        for(int i = 0; i < images.length; i++) {
+            g.drawImage(images[i], imagesPos[i][0], imagesPos[i][1], null);
+        }
+    }
+    
     void drawPoly(int[] pointsToDraw) {
         points = pointsToDraw;
         repaint();
     }
+
     BufferedImage[] images;
-    void drawImg(BufferedImage[] imagesToDraw) {
+    int[][] imagesPos;
+    void drawImg(BufferedImage[] imagesToDraw, int[][] pos) {
+        this.imagesPos = pos;
         this.images = imagesToDraw;
         repaint();
     }
 
-    private void loadImg() {
-        //return img;
+    void refresh() {
+        repaint();
     }
 
 }
