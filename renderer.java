f0b2c5ac09191c8e7d90aa9e3dbdd7e90cc53194
Spitfire-404 <saltenisu@gmail.com>
2024-07-28 04:45:52 -0400

    changed the type of window for performance

--- renderer.java
+++ renderer.java
@@ -2,9 +2,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import javax.swing.*;
 
-public class renderer extends JFrame{
+public class renderer extends Frame{
 
     // creating a keyadapter object to listen for key events
     KeyAdapter keyAdapter = new KeyAdapter() {
@@ -12,25 +11,53 @@ public class renderer extends JFrame{
         // override the keypressed method(allow us to do things with keys are pressed)
         @Override
         public void keyPressed(KeyEvent e) {
-            System.out.println(e.getKeyCode());
             if(e.getKeyCode() == KeyEvent.VK_R) {// call the repaint method
                 repaint();
             }
-
             if(e.getKeyCode() == KeyEvent.VK_W) {// up
-                imagesPos[0][1] -= 10;
+                if(points != null) {
+                    for(int i = 1; i< points.length; i+=2) {
+                        points[i] -= 10;
+                    }
+                }
+
+                if (images != null) {
+                    imagesPos[0][1] -= 10;
+                }
                 repaint();
             }
             if(e.getKeyCode() == KeyEvent.VK_S) {// down
-                imagesPos[0][1] += 10;
+                if(points != null) {
+                    for(int i = 1; i< points.length; i+=2) {
+                        points[i] += 10;
+                    }
+                }
+                if (images != null) {
+                    imagesPos[0][1] += 10;
+                }
                 repaint();
             }
             if(e.getKeyCode() == KeyEvent.VK_A) {// left
-                imagesPos[0][0] -= 10;
+                if(points != null) {
+                    for(int i = 0; i< points.length; i+=2) {
+                        points[i] -= 10;
+                    }
+                }
+
+                if (images != null) {
+                    imagesPos[0][0] -= 10;
+                }
                 repaint();
             }
             if(e.getKeyCode() == KeyEvent.VK_D) {// right
-                imagesPos[0][0] += 10;
+                if(points != null) {
+                    for(int i = 0; i< points.length; i+=2) {
+                        points[i] += 10;
+                    }
+                }
+                if (images != null) {
+                    imagesPos[0][0] += 10;
+                }
                 repaint();
             }
         }
@@ -47,11 +74,11 @@ public class renderer extends JFrame{
         this.width = width; // set the width
         this.height = height; // set the height
         setSize(width, height); // set the size of the window
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // ???
+        setResizable(false); // make the window not resizable
         setLocationRelativeTo(null); // set the window in the middle of the screen
         setVisible(true); // make the window visible
 
-        this.addKeyListener(keyAdapter);
+        addKeyListener(keyAdapter);
     }
 
     private int[] points; // points to draw
@@ -66,13 +93,17 @@ public class renderer extends JFrame{
         g2d = (Graphics2D) g; // cast the graphics object to a graphics 2d object
         
         // draw the lines
-        for(int i = 0; i < points.length-2; i += 2) {
-            g2d.drawLine(points[i], points[i+1], points[i+2], points[i+3]);
+        if(points != null) {
+            for(int i = 0; i < points.length-2; i += 2) {
+                g2d.drawLine(points[i], points[i+1], points[i+2], points[i+3]);
+            }
         }
 
         // draw the images
-        for(int i = 0; i < images.length; i++) {
-            g2d.drawImage(images[i], imagesPos[i][0], imagesPos[i][1], null);
+        if(this.images != null) {
+            for(int i = 0; i < images.length; i++) {
+                g2d.drawImage(images[i], imagesPos[i][0], imagesPos[i][1], null);
+            }
         }
     }
     
